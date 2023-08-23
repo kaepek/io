601eee5e20e7da2f88cf913303c7169d00eece5a
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-08-23 14:51:44 +0100

    working on inheritance

--- lib/c++/comlib.cpp
+++ lib/c++/comlib.cpp
@@ -1,11 +1,11 @@
-#include "comlib.h"
+#include "comlib.hpp"
 
 namespace kaepek
 {
     template <typename T, std::size_t MAX_BUFFER_SIZE>
     SerialInputControl<T, MAX_BUFFER_SIZE>::SerialInputControl(T *device_instance)
+        : device_instance(device_instance)
     {
-        this->device_instance = device_instance;
     }
 
     template <typename T, std::size_t MAX_BUFFER_SIZE>
@@ -16,23 +16,25 @@ namespace kaepek
         {
             // we have a byte.
             char next_byte = Serial.read();
-            if (!control_word_set) {
+            if (!control_word_set)
+            {
                 // next byte is a word.
                 current_control_word = next_byte;
                 control_word_set = true;
             }
-            else {
+            else
+            {
                 // we are working on a word already, handle the byte
                 control_word_data_buffer[buffer_idx] = next_byte;
                 buffer_idx++;
-                if (buffer_idx > serial_input_command_word_buffer_size[current_control_word]) {
+                if (buffer_idx > serial_input_command_word_buffer_size[current_control_word])
+                {
                     control_word_set = false;
                     device_instance->process_host_control_word(current_control_word, control_word_data_buffer);
                 }
             }
         }
         sei(); // enable interrupt.
-        
     }
 
     template <typename T, std::size_t MAX_BUFFER_SIZE>
--- lib/com-test/com-test.ino
+++ lib/com-test/com-test.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "lib/comlib.cpp"
 
+namespace kaepek
+{
+  class BaseTester {
+    public:
+     BaseTester() {}
+  };
+
+  class ComTester: public BaseTester
+  {
+  private:
+    SerialInputControl<ComTester, 2> serialInputControl;
+
+  public:
+    ComTester()
+        : BaseTester(), 
+        serialInputControl(this)
+    {
+    }
+
+    void process_host_control_word(uint32_t control_word, uint32_t *data_buffer)
+    {
+      Serial.print("Received control word:");
+      Serial.println(control_word);
+    }
+
+    void run()
+    {
+      serialInputControl.read_input();
+    }
+  };
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+kaepek::ComTester device;
 
+void setup()
+{
+}
+
+void loop()
+{
+  device.run();
 }
