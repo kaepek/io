81c18e558e0acfe24b19a9ce2b4fa1fb77dc8f08
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-08-28 22:32:55 +0100

    debugging and adding nots

--- lib/host/controller/input-output-device-controllers/index.ts
+++ lib/host/controller/input-output-device-controllers/index.ts
@@ -1,10 +1,12 @@
-import { ConsoleDeviceController } from "./console"
+import { ConsoleDeviceController } from "./handlers/console"
 import { NetworkControlWordSink } from "./handlers/network-control-word-sink"
-import { SerialUSBDeviceController } from "./serial-usb"
+import { SerialUSBDeviceController } from "./handlers/serial-usb"
+import { InputOutputDeviceControllerBase } from "./model"
 
-
-export default {
+const exports: {[key: string]: typeof InputOutputDeviceControllerBase | typeof NetworkControlWordSink } = {
     "console": ConsoleDeviceController,
     "serial-usb": SerialUSBDeviceController,
     "network": NetworkControlWordSink
-}
+};
+
+export default exports;
--- lib/host/controller/scripts/cli.ts
+++ lib/host/controller/scripts/cli.ts
@@ -4,14 +4,20 @@ import { ControlSourceInputRouter } from "../control-input-sources/router";
 import ControlWords from "../control-words/index";
 import { ControlWordHandlerBase } from "../control-words/model";
 import InputOutputDevices from "../input-output-device-controllers/index";
+import { InputOutputDeviceControllerBase } from "../input-output-device-controllers/model";
 import DeviceOuputSinks from "../output-sinks/index"
+import { OutputSinkBase } from "../output-sinks/model";
 import { DeviceOutputRouter } from "../output-sinks/router";
 import { parseArgs } from "node:util";
+import { StreamJunctionDirector } from "../stream-junction-director";
+import { DelimitedASCIILine } from "../device-output-models/handlers/delimited-ascii-line";
+import { DeviceOutputModelBase } from "../device-output-models/model";
 
 function parse_concept<T>(parsed_args_concept_values: Array<string>, concept_name: string, concept_index: any) {
     // parse control words
     const concept_items = [] as Array<T>;
     parsed_args_concept_values.forEach((source_str) => {
+        // maybe check for comma syntax so so split by this first in case of -c start,stop rather than -c start -c stop no do a pipe symbol instead
         const source_str_split = source_str.split("=");
         if (source_str_split.length > 2) {
             throw `Too many '=' key value delimiters for ${concept_name}: ${JSON.stringify(parsed_args_concept_values)}`;
@@ -22,6 +28,7 @@ function parse_concept<T>(parsed_args_concept_values: Array<string>, concept_nam
             args = source_str_split[1].split(",");
         }
         const ConceptHandler = concept_index[word_name];
+        console.log("ConceptHandler", concept_index, ConceptHandler, word_name);
         let wordHandlerInstance = null;
         if (args.length) {
             wordHandlerInstance = new (ConceptHandler as new (...args: any[]) => T)(...args);
@@ -55,7 +62,8 @@ async function start_cli() {
             },
             sink: {
                 type: "string",
-                short: "o"
+                short: "o",
+                multiple: true
             }
 
         }
@@ -68,55 +76,28 @@ async function start_cli() {
         (parsed_args.values["peripheral"] && parsed_args.values["peripheral"].length)
     ) {
         // parse source handlers
-        const sources = [] as Array<ControlInputSourceHandler>;
-        parsed_args.values["source"].forEach((source_str) => {
-            const source_str_split = source_str.split("=");
-            if (source_str_split.length > 2) {
-                throw "Too many '=' key value delimiters for source: " + JSON.stringify(parsed_args);
-            }
-            let command_name = source_str_split[0];
-            let args: Array<string> = [];
-            if (source_str_split.length == 2) { // has arguments
-                args = source_str_split[1].split(",");
-            }
-            const SourceHandler = ControlInputSources[command_name];
-            let sourceHandlerInstance = null;
-            if (args.length) {
-                sourceHandlerInstance = new (SourceHandler as new (...args: any[]) => ControlInputSourceHandler)(...args);
-            }
-            else {
-                sourceHandlerInstance = new (SourceHandler as unknown as new () => ControlInputSourceHandler)();
-            }
-            sources.push(sourceHandlerInstance);
-        });
+        const sources = parse_concept<ControlInputSourceHandler>(parsed_args.values["source"], "source", ControlInputSources);
         // parse control words
-        const words = [] as Array<ControlWordHandlerBase>;
-        parsed_args.values["control_word"].forEach((source_str) => {
-            const source_str_split = source_str.split("=");
-            if (source_str_split.length > 2) {
-                throw "Too many '=' key value delimiters for control_word: " + JSON.stringify(parsed_args);
-            }
-            let word_name = source_str_split[0];
-            let args: Array<string> = [];
-            if (source_str_split.length == 2) { // has arguments
-                args = source_str_split[1].split(",");
-            }
-            const WordHandler = ControlWords[word_name];
-            let wordHandlerInstance = null;
-            if (args.length) {
-                wordHandlerInstance = new (WordHandler as new (...args: any[]) => ControlWordHandlerBase)(...args);
-            }
-            else {
-                wordHandlerInstance = new (WordHandler as unknown as new () => ControlWordHandlerBase)();
-            }
-            words.push(wordHandlerInstance);
-        });
+        const words =  parse_concept<ControlWordHandlerBase>(parsed_args.values["control_word"], "control_word", ControlWords);
+        // parse io devices
+        const devices = parse_concept<InputOutputDeviceControllerBase>(parsed_args.values["peripheral"], "peripheral", InputOutputDevices);
+        // parse sinks
+        const sinks = parse_concept<OutputSinkBase>(parsed_args.values["sink"] || [], "sink", DeviceOuputSinks);
+        // fixed model atm
+        const model = new DelimitedASCIILine();
 
+        const inputSourceRouter = new ControlSourceInputRouter(sources);
+        const outputRouter = new DeviceOutputRouter(sinks);
+        const director_instance = new StreamJunctionDirector(inputSourceRouter, words, devices, model, outputRouter);
 
         console.log("sources", sources);
         console.log("words", words);
+        console.log("devices", devices);
+        console.log("sinks", sinks);
+        console.log("model", model);
+        console.log(director_instance);
 
-
+        return director_instance.ready();
     }
     else {
         throw "Not enough arguments: " + JSON.stringify(parsed_args);
