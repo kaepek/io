5304fda7af0a667fd4e2f4c120f645f4a515fd7d
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-08-29 15:05:21 +0100

    working on director errors

--- lib/host/controller/scripts/director.ts
+++ lib/host/controller/scripts/director.ts
@@ -1,6 +1,6 @@
-#!/bin/sh 
+#!/bin/sh
 ":" //# https://sambal.org/?p=1014; NODE_OPTIONS=--experimental-vm-modules NODE_NO_WARNINGS=1 exec /usr/bin/env node --experimental-loader=extensionless "$0" "$@"
-;
+    ;
 import ControlInputSources from "../control-input-sources/index";
 import { ControlInputSourceHandler } from "../control-input-sources/model";
 import { ControlSourceInputRouter } from "../control-input-sources/router";
@@ -47,7 +47,7 @@ function parse_concept<T>(parsed_args_concept_values: Array<string>, concept_nam
         // maybe check for comma syntax so so split by this first in case of -c start,stop rather than -c start -c stop no do a pipe symbol instead
         const source_str_split = source_str.split("=");
         if (source_str_split.length > 2) {
-            throw `Too many '=' key value delimiters for ${concept_name}: ${JSON.stringify(parsed_args_concept_values)}`;
+            throw `Director error: Too many '=' key value delimiters for ${concept_name}: ${JSON.stringify(parsed_args_concept_values)}`;
         }
         let concept_handler_name = source_str_split[0];
         let args: Array<string> = [];
@@ -57,7 +57,7 @@ function parse_concept<T>(parsed_args_concept_values: Array<string>, concept_nam
         const ConceptHandler = concept_index[concept_handler_name];
 
         if (!ConceptHandler) {
-            throw `ConceptHandler for concept ${concept_name} with name ${concept_handler_name} is missing. Options are ${Object.keys(concept_index)}`;
+            throw `Director error: ConceptHandler for concept ${concept_name} with name ${concept_handler_name} is missing. Options are ${Object.keys(concept_index)}`;
         }
         let wordHandlerInstance = null;
         if (args.length) {
@@ -93,28 +93,27 @@ function custom_parse_args() {
     ];
 
     const args = process.argv;
-    args.splice(0,2);
-    const command_words_short: {[name: string]: any} = {};
-    const command_words_long: {[name: string]: any} = {};
+    args.splice(0, 2);
+    const command_words_short: { [name: string]: any } = {};
+    const command_words_long: { [name: string]: any } = {};
 
     if (parse_options.options) {
         const options = parse_options.options as any;
         Object.keys(options).forEach((option_name) => {
-            command_words_long[`--${option_name}`] = {...options[option_name], option_name};
-            if (options[option_name].short)
-            {
-                command_words_short[`-${options[option_name].short}`] = {...options[option_name], option_name};
+            command_words_long[`--${option_name}`] = { ...options[option_name], option_name };
+            if (options[option_name].short) {
+                command_words_short[`-${options[option_name].short}`] = { ...options[option_name], option_name };
             }
         });
     };
 
-    const parsed_args: {values: {[key:string]: Array<string>}} = {values:{}};    
+    const parsed_args: { values: { [key: string]: Array<string> } } = { values: {} };
     let current_arg_name: string | null = null;
 
     args.forEach((arg_str_segment) => {
         const possible_arg = command_words_short[arg_str_segment] || command_words_long[arg_str_segment];
         if (possible_arg) {
-            const arg_name = possible_arg.option_name; 
+            const arg_name = possible_arg.option_name;
             if (!parsed_args.values.hasOwnProperty(arg_name)) {
                 parsed_args.values[arg_name] = [];
             }
@@ -164,7 +163,17 @@ async function start_cli() {
         return director_instance.ready();
     }
     else {
-        throw "Not enough arguments: " + JSON.stringify(parsed_args);
+
+        const missing_options: Array<string> = [];
+        Object.keys(parse_options.options as any).forEach((option_name) => {
+            if (!parsed_args.values[option_name] && option_name !== "sink") {
+                missing_options.push(option_name);
+            }
+        });
+        throw `Director error: Missing the following arguments. Need the following ${missing_options.map(option_str => {
+            const option = (parse_options.options as any)[option_str];
+            return `--${option_str} or -${option.short}`
+        }).join(", ")}. --sink or -o argument is optional.`;
     }
 }
 
