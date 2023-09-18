9d97ed1295da1601e36fafc6fb51202fa7ab55d8
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-09-18 13:58:24 +0100

    adding task implementation

--- lib/host/ts-adaptors/tasks.ts
+++ lib/host/ts-adaptors/tasks.ts
@@ -1,3 +1,33 @@
+import { Observable, Subscription } from "rxjs";
+
+
 export class Task {
-    
+
+    tick(incoming_data: any) {
+        throw "Not implemented yet";
+    }
+
+    private return_promise_resolver: ((value: void | PromiseLike<void>) => void) | null = null;
+    private return_promise_rejector: ((reason?: any) => void) | null = null;
+
+    async run() {
+        return this.return_promise;
+    }
+
+    return_promise: Promise<void> | null;
+    input_subscription: Subscription;
+
+    constructor(input$: Observable<any>) {
+        this.input_subscription = input$.subscribe((data) => this.tick(data));
+        this.return_promise = new Promise<void>((resolve, reject) => {
+            this.return_promise_resolver = resolve;
+            this.return_promise_rejector = reject;
+        }).then((ret: any) => {
+            this.input_subscription.unsubscribe();
+            return ret;
+        }).catch((err: any) => {
+            this.input_subscription.unsubscribe();
+            return err;
+        });
+    }
 }
