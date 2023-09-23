3f3074a7ec6e5a50a82ce22c8396a3b5d140ff29
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-09-23 13:44:00 +0100

    changing merging strategy for task runner state merging

--- lib/host/ts-adaptors/task-runner.ts
+++ lib/host/ts-adaptors/task-runner.ts
@@ -1,6 +1,16 @@
 import NetworkAdaptor from "./network.js";
 import { Task } from "./task.js";
 
+const merge = (o1: any, o2: any) => {
+    const c1 = structuredClone(o1);
+    const c2 = structuredClone(o2);
+    Object.keys(c2).forEach((key) => {
+        if (c2[key] instanceof Object && c1[key] instanceof Object) return c1[key] = merge(c1[key], c2[key]);
+        c1[key] = c2[key];
+    });
+    return c1;
+};
+
 export async function run_tasks(tasks: Array<Task<any>>, adaptor: NetworkAdaptor) {
     let state = {};
 
@@ -12,7 +22,7 @@ export async function run_tasks(tasks: Array<Task<any>>, adaptor: NetworkAdaptor
     tasks.forEach((task) => {
         resolver = resolver.then(async () => {
             const return_prom = task.completed().then((state_data_additions) => {
-                state = { ...state_data_additions, ...state };
+                state = merge(state, state_data_additions);
                 return state;
             });
             await task.run(state);
